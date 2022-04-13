107279dff7e9d6257f1f1f3c6da27759ab5bd7fe
Javier Duarte <jduarte@ucsd.edu>
2022-04-13 14:40:48 -0700

    bops and weight bits

--- make_qonnx_graph.py
+++ make_qonnx_graph.py
@@ -13,23 +13,26 @@ def nonlinear(x):
     # return 1e-4*np.power(x,1/2)
 
 
-# Model name -> Dataset & Accuracy & No. of MACs & No. of weights
+# Model name -> Dataset, Accuracy, MACs, BOPs, Weights, Total weight bits
 input_dict = OrderedDict(
     [
-        ("MobiletNet-w4a4", ["ImageNet", 71.14, 557381408, 4208224]),
-        ("CNV-w1a1", ["CIFAR-10", 84.22, 57906176, 1542848]),
-        ("CNV-w1a2", ["CIFAR-10", 87.80, 57906176, 1542848]),
-        ("CNV-w2a2", ["CIFAR-10", 89.03, 57906176, 1542848]),
-        ("TFC-w1a1", ["MNIST", 93.17, 59008, 59008]),
-        ("TFC-w1a2", ["MNIST", 94.79, 59008, 59008]),
-        ("TFC-w2a2", ["MNIST", 96.60, 59008, 59008]),
+        (
+            "MobiletNet-w4a4",
+            ["ImageNet", 71.14, 557381408, 74070028288, 4208224, 16839808],
+        ),
+        ("CNV-w1a1", ["CIFAR-10", 84.22, 57906176, 107672576, 1542848, 1542848]),
+        ("CNV-w1a2", ["CIFAR-10", 87.80, 57906176, 165578752, 1542848, 1542848]),
+        ("CNV-w2a2", ["CIFAR-10", 89.03, 57906176, 331157504, 1542848, 3085696]),
+        ("TFC-w1a1", ["MNIST", 93.17, 59008, 59008, 59008, 59008]),
+        ("TFC-w1a2", ["MNIST", 94.79, 59008, 118016, 59008, 59008]),
+        ("TFC-w2a2", ["MNIST", 96.60, 59008, 236032, 59008, 118016]),
     ]
 )
 
 labels = input_dict.keys()
 y = np.array([input_dict[key][1] for key in input_dict])
-x = np.array([input_dict[key][2] for key in input_dict])
-w = np.array([input_dict[key][3] for key in input_dict])
+x = np.array([input_dict[key][3] for key in input_dict])
+w = np.array([input_dict[key][5] for key in input_dict])
 
 colors = [
     "#1f77b4",
@@ -60,14 +63,17 @@ for xi, yi, wi, l, c in zip(x, y, w, labels, colors):
     ax.plot([xi], [yi], label=l, marker="o", markersize=nonlinear(wi), color=c)
     ax.plot([xi], [yi], label=l, marker="*", markersize=10, color="white")
     if "TFC" in l:
-        ax.text(xi * 1.5, yi - 0.3, l, color=c)
+        ax.text(xi * 1.7, yi - 0.3, l, color=c)
     elif "CNV" in l:
-        ax.text(xi * 0.01, yi - 0.5, l, color=c)
+        if "w2a2" in l:
+            ax.text(xi * 5, yi - 0.5, l, color=c)
+        else:
+            ax.text(xi * 0.005, yi - 0.5, l, color=c)
     else:
-        ax.text(xi * 0.0013, yi - 3, l, color=c)
+        ax.text(xi * 0.0002, yi - 3, l, color=c)
 
 axs[0].plot(
-    [1e4],
+    [1e5],
     [75],
     label="100 k",
     marker="o",
@@ -75,7 +81,7 @@ axs[0].plot(
     color="gray",
 )
 axs[0].plot(
-    [1e5 / 2.5],
+    [5e5],
     [75],
     label="1 M",
     marker="o",
@@ -83,20 +89,22 @@ axs[0].plot(
     color="gray",
 )
 axs[0].plot(
-    [1e6 / 2.5],
+    [1e7],
     [75],
-    label="5 M",
+    label="10 M",
     marker="o",
-    markersize=nonlinear(5e6),
+    markersize=nonlinear(10e6),
     color="gray",
 )
-axs[0].text(5.5e3, 76.5, "100 k", color="black", size=18)
-axs[0].text(6e4 / 2.5, 75 - 0.5, "1 M", color="white", size=18)
-axs[0].text(6e5 / 2.5, 75 - 0.5, "5 M", color="white", size=18)
+
+axs[0].text(5e4, 77.8, "Weight bits", color="gray", size=20)
+axs[0].text(1e5, 75, "100 kb", color="white", size=10, ha="center", va="center")
+axs[0].text(5e5, 75, "1 Mb", color="white", size=18, ha="center", va="center")
+axs[0].text(1e7, 75, "10 Mb", color="white", size=18, ha="center", va="center")
 
 
-xmin = 3e3
-xmax = 1e10
+xmin = 2e4
+xmax = 2e12
 for ax in axs:
     ax.semilogx()
 
@@ -106,7 +114,7 @@ axs[1].set_xlim(xmin, xmax)
 axs[1].set_ylim(81, 92)
 axs[2].set_xlim(xmin, xmax)
 axs[2].set_ylim(92, 98)
-axs[2].set_xlabel("MACs")
+axs[2].set_xlabel("BOPs")
 axs[1].set_ylabel("Accuracy [%]\nCIFAR-10")
 axs[0].set_ylabel("\nImageNet")
 axs[2].set_ylabel("\nMNIST")
